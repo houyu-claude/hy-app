2e51be71490e580364188c6b5824796608f1db03
zhongxiaoyi738 <zhongxiaoyi738@163.com>
2026-05-19 17:26:11 +0800

    chore: 技术栈优化 - 升级依赖版本、合并类型声明、更新配置

--- src/shims-uni.d.ts
+++ src/shims-uni.d.ts
@@ -1,10 +1,16 @@
 /// <reference types='@dcloudio/types' />
 import 'vue'
 
+export {}
+
 declare module '@vue/runtime-core' {
   type Hooks = App.AppInstance & Page.PageInstance;
 
   interface ComponentCustomOptions extends Hooks {
-
   }
+}
+
+declare module "vue" {
+  type Hooks = App.AppInstance & Page.PageInstance;
+  interface ComponentCustomOptions extends Hooks {}
 }
